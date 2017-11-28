2d3ecba4302808c8c19983583d185530442bdc09
Bruno Krebs <bruno.krebs@auth0.com>
2017-11-28 10:41:57 -0200

    Basic Express/Node.js API

---
+++ index.js
@@ -0,0 +1,23 @@
+// importing dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// configuring Express
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// defining contacts array and endpoints to manipulate it
+const contacts = [
+    { name: 'Bruno Krebs', phone: '+555133334444' },
+    { name: 'John Doe', phone: '+191843243223' }
+];
+app.get('/contacts', (req, res) => res.send(contacts));
+app.post('/contacts', (req, res) => {
+    contacts.push(req.body);
+    res.send();
+});
+
+// starting Express
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
